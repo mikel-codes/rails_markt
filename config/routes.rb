614c6d79ac40da3a4410dd76a647837b0ec940b8
smeez <gatez@yahoo.com>
2020-10-04 23:49:53 +0100

    device model setup and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+require 'api_constraints'
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   #api definition
   namespace :api, defaults: {format: :json} , constraints: {subdomain: 'api'} do
-    scope module: :v1, constraints: ApiConstraints(version: 1, default: true) do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       
     end
   end
