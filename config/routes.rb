841ee4e98739ec57b5e5f3cea104088b455641da
smeez <gatez@yahoo.com>
2020-10-07 17:31:41 +0100

    added routes for user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 require 'api_constraints'
+
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   #api definition
   namespace :api, defaults: {format: :json} , constraints: {subdomain: 'api'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      
+      resources :users, only: [:show, :update]
     end
   end
 end
