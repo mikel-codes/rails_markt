93414c138ce5b419209f7aad0cde79ca51602631
smeez <gatez@yahoo.com>
2020-10-07 17:42:26 +0100

    nothing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'database_cleaner'
-require 'rspec/rails'
 
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
+require 'rspec/rails'
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -45,6 +45,8 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
   #before uploading this whole speck page run a database cleaner 
   # this will clean a test_db with truncation sql call
   # using a transaction sql command which seems faster 
