7df8a4fc91f96e036e4fe18ac972a195075c736f
smeez <gatez@yahoo.com>
2020-10-07 06:25:30 +0100

    add actions to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::UsersController < ApplicationController
+    #GET
+    def show
+        render json: User.find(params[:id])
+    end
+
+    #PUT/PATCH
+    def update
+        
+    end
 end
