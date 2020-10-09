fe35700ff4fb79758bbae317011e04b4729e6fc3
smeez <gatez@yahoo.com>
2020-10-09 05:24:43 +0100

    create the create of the model lifecycle

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,8 +4,23 @@ class Api::V1::UsersController < ApplicationController
         render json: User.find(params[:id])
     end
 
+    def create
+        user = User.new user_params
+        if user.valid?
+            user.save!
+            render json: user, status: 201, location: [:api, user]
+        else
+            render json: user.errors, status: 422
+        end
+    end
+
     #PUT/PATCH
     def update
         
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:id, :email, :password, :password_confirmation)
     end
 end
