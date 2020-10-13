041c62d6818669bdf5a18102f2094b0f3409bfe0
smeez <gatez@yahoo.com>
2020-10-13 23:39:24 +0100

    removed private mthd declaration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-  before_create :generate_authencation_token!
+  before_create :generate_authentication_token!
   validates :auth_token , uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  private 
-  def generate_authencation_token!
+  def generate_authentication_token!
     begin
       self.auth_token = Devise.friendly_token
     end while self.class.exists?(auth_token: auth_token)
