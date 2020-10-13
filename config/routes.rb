590bf2ea8a04caf63795d9a16f5c911c3f2d144a
smeez <gatez@yahoo.com>
2020-10-13 23:42:45 +0100

    sessions and tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  #api definition
+
   namespace :api, defaults: {format: :json} , constraints: {subdomain: 'api'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :update, :create, :destroy]
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe User, type: :model do
 
   describe "generate authentication token" do
     it "generates a unique token" do
-      Devise.stub(:friendly_token) do
+      Devise.stub(:friendly_token).and_return "auniquetoken123" do
         @user.generate_authentication_token!
         expect(@user.auth_token).to eql "auniquetoken123"
       end
     end
 
-    #it "generates another token when one already has been  taken" do
-     # existing_user = FactoryBot.create :user, auth_token: "auniquetoken123"
-     # @user.generate_authentication_token!
-     # expect(@user.auth_token).not_to eql existing_user.auth_token
-    #end
+    it "generates another token when one already has been  taken" do
+      existing_user = FactoryBot.create :user, auth_token: "auniquetoken123"
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
   end
 
 end
