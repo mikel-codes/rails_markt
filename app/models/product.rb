f44bb68e3de0ba7804dcb700659c22b2e12d306a
smeez <gatez@yahoo.com>
2020-10-16 21:23:27 +0100

    change validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
   
-  validates :title, :user_id, :price, :published,  :presence => true
+  validates :title, :user_id, :price :presence => true
   validates :price, numericality: {greater_than_or_equal_to: 0}
 end
